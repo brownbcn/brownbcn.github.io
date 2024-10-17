498b8038dde3665a7ef892255703692b81470ae9
brownbcn <morllumarc@gmail.com>
2024-10-17 13:39:21 +0200

    web en construccion

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@
 console.log("Página cargada correctamente");
 
 
+window.onload = function() {
+    document.getElementById("popup").classList.add("popup-show");
+
+    // Cerrar el pop-up después de 5 segundos (5000 milisegundos)
+    setTimeout(closePopup, 5000);
+};
+
